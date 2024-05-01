1be7fbc2b673a5e8faeb2bf9ccc8d2981320ac01
bmm522 <rlawldls11@naver.com>
2024-05-02 00:01:27 +0900

    feat : 공통 레이아웃 추가

--- crossfit-admin-front/src/app/layout.tsx
+++ crossfit-admin-front/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import CommonLayout from "@/layouts/CommonLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>
-          레이아웃
-        </div>
-        {children}</body>
+        <CommonLayout>
+          {children}
+        </CommonLayout>
+      </body>
     </html>
   );
 }
